55af359ed9d0f918349cce50a6211513eed981ef
Edimar Pires <edimarfilho971@gmail.com>
2023-06-15 14:39:32 -0300

    Configurando Anbiente Front e Back

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,19 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify()
 const port = 333
+//Realizando conecsão com o BD
+const Prisma = new PrismaClient()
 
-app.listen({port}).then(() => {
+app.get('/users', async () => {
+    //findMany : Metodo para trazer varios usuarios
+    const users = await Prisma.user.findMany()
+
+    return users
+})
+
+app.listen({port}).then( () => 
+{
     console.log("Servidor funcionando👌")
 })
